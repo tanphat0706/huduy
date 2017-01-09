895344c1da8a910dc5b68748377e59b8d724de59
Phat Le <letanphat0706@gmail.com>
2017-01-09 23:48:34 +0700

    update template

--- public/js/main_front.js
+++ public/js/main_front.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function($){
                 items:3,
             },
             1000:{
-                items:5,
+                items:4,
             }
         }
     });  
